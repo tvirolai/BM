4931edcd97695b0a7ece79839a5f848c11050016
Tuomo Virolainen <tuomo.virolainen@apsi.fi>
2015-06-08 21:47:14 +0300

    Album URL scraping added

--- scrape_lyrics.js
+++ scrape_lyrics.js
@@ -3,87 +3,62 @@
 
 var request = require('request');
 var fs = require('fs');
+var cheerio = require('cheerio');
+var relative_urls = [];
+var lyrics_urls = [];
+var urlListFile = 'List_of_verified_band_URLS.txt';
+var urls = [];
 
-var file = 'List_of_black_metal_bands.txt';
-var url = 'http://www.darklyrics.com/';
-var bands = [];
-var band_urls = [];
-var verifiedUrls = [];
+getBandUrls();
 
-var normalizationMap = {
-	'å': 'a',
-	'ä': 'a',
-	'ă': 'a',
-	'ö': 'a',
-	'-': '',
-	'.': '',
-	'\'': '',
-	' ': '',
-	'ï': 'i',
-	'ó': 'o',
-	'ø': 'o',
-	'á': 'a',
-	'ü': 'u',
-	')': ''
+function getBandUrls () {
+    fs.readFile(urlListFile, 'utf-8', function doneReading(err, contents) {
+        if (!err) {
+            urls = contents.split('\n');
+        }
+        haku(parseURLs);
+    });
+}
+
+function haku(callback) {
+    var pageCount = 0;
+    for (var i = 0; i < urls.length; i++) {
+        request(urls[i], function (err, res, body) {
+            if (!err) {
+                var $ = cheerio.load(body);
+                pageCount += 1;
+                console.log(pageCount + " / " + urls.length + " pages scraped...");
+                $( "#main > div.cntwrap > div a" ).each(function (index, element) {
+                    var link = $(this).attr('href');
+                    relative_urls.push(link);
+                });
+                relative_urls.sort();
+                if (pageCount == urls.length) {
+                    if (callback) {
+                        callback();
+                    }
+                }
+            } else {
+                console.log("Error, error.");
+            }
+
+        });
+
+    }
 };
 
 
-function bandlist(callback) {
-	fs.readFile(file, 'utf-8', function doneReading(err, contents) {
-		if (!err) {
-			bands = contents.split('\n');
-		}
-		callback();
-	});
-}
-
-function parseUrls() {
-	for (var i = 0; i < bands.length; i++) {
-		var band = normalizer(bands[i]);
-		var initial = band[0];
-		if (! isNaN(initial) ) {
-			initial = '19';
-		}
-		var band_url = url + initial + '\/' + band + '.html';
-		band_urls.push(band_url);
-	}
-	band_urls.forEach(function (url, index) {
-		verify_URL(url, index);
-	});
-}
-
-function result() {
-	var percentage = Math.round(verifiedUrls.length / band_urls.length * 100);
-	console.log(percentage + ' % (' + verifiedUrls.length + '/' + band_urls.length + ') of the contructed URLs work.');
-	var verifiedUrls_as_string = verifiedUrls.join('\n');
-	fs.writeFile('List_of_verified_band_URLS.txt', verifiedUrls_as_string);
-}
-
-function verify_URL (url, index) {
-	request(url, function (err, res) {
-		if (res.statusCode === 200) {
-			console.log(url + ' works!');
-			verifiedUrls.push(url);
-			if (index == band_urls.length - 1) {
-				result();
-			}
-		} else {
-			console.log(url + ' does not exist.');
-		}
-	});
-}
-
-function normalizer (string) {
-	var s = string.toLowerCase();
-	var normalizedString = '';
-	for (var i = 0; i < s.length; i++) {
-		if (normalizationMap[s[i]] !== undefined) {
-			normalizedString += normalizationMap[s[i]];
-		} else {
-			normalizedString += s[i];
-		}
-	}
-	return normalizedString;
-}
-
-bandlist(parseUrls);
+function parseURLs () {
+    relative_urls.forEach(function (value, index) {
+        var parsed_url = 'http://www.darklyrics.com' + value.substring(2, value.indexOf('\#'));
+        if (lyrics_urls.indexOf(parsed_url) < 0) {
+            console.log("Writing: " + parsed_url);
+            lyrics_urls.push(parsed_url);
+        }
+        if (index == relative_urls.length - 1) {
+            var urls_as_string = lyrics_urls.join('\n');
+            fs.writeFile('List_of_album_lyric_URLS.txt', urls_as_string);
+            console.log(lyrics_urls.length + " album URLs written to file.");
+        }
+    });
+};
