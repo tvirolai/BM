1375d68a096544846abdb3825c852f1801e38fdf
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-06-16 09:45:16 +0300

    Fixed indentation

--- parse_URLs.js
+++ parse_URLs.js
@@ -11,82 +11,82 @@ var band_urls = [];
 var verifiedUrls = [];
 
 var normalizationMap = {
-	'å': 'a',
-	'ä': 'a',
-	'ă': 'a',
-	'ö': 'a',
-	'-': '',
-	'.': '',
-	'\'': '',
-	' ': '',
-	'ï': 'i',
-	'ó': 'o',
-	'ø': 'o',
-	'á': 'a',
-	'ü': 'u',
-	')': ''
+  'å': 'a',
+  'ä': 'a',
+  'ă': 'a',
+  'ö': 'a',
+  '-': '',
+  '.': '',
+  '\'': '',
+  ' ': '',
+  'ï': 'i',
+  'ó': 'o',
+  'ø': 'o',
+  'á': 'a',
+  'ü': 'u',
+  ')': ''
 };
 
 
 function bandlist(callback) {
-	fs.readFile(file, 'utf-8', function doneReading(err, contents) {
-		if (!err) {
-			bands = contents.split('\n');
-		}
-		callback();
-	});
+  fs.readFile(file, 'utf-8', function doneReading(err, contents) {
+    if (!err) {
+      bands = contents.split('\n');
+    }
+    callback();
+  });
 }
 
 function parseUrls() {
-	for (var i = 0; i < bands.length; i++) {
-		var band = normalizer(bands[i]);
-		var initial = band[0];
-		if (! isNaN(initial) ) {
-			initial = '19';
-		}
-		var band_url = url + initial + '\/' + band + '.html';
-		band_urls.push(band_url);
-	}
-	band_urls.forEach(function (url, index) {
-		verify_URL(url, index);
-	});
+  for (var i = 0; i < bands.length; i++) {
+    var band = normalizer(bands[i]);
+    var initial = band[0];
+    if (! isNaN(initial) ) {
+      initial = '19';
+    }
+    var band_url = url + initial + '\/' + band + '.html';
+    band_urls.push(band_url);
+  }
+  band_urls.forEach(function (url, index) {
+    verify_URL(url, index);
+  });
 }
 
 function result () {
-	var percentage = Math.round(verifiedUrls.length / band_urls.length * 100);
-	console.log(percentage + ' % (' + verifiedUrls.length + '/' + band_urls.length + ') of the contructed URLs work.');
-	var verifiedUrls_as_string = verifiedUrls.join('\n');
-	fs.writeFile('List_of_verified_band_URLS.txt', verifiedUrls_as_string);
+  var percentage = Math.round(verifiedUrls.length / band_urls.length * 100);
+  console.log(percentage + ' % (' + verifiedUrls.length + '/' + band_urls.length + ') of the contructed URLs work.');
+  var verifiedUrls_as_string = verifiedUrls.join('\n');
+  fs.writeFile('List_of_verified_band_URLS.txt', verifiedUrls_as_string);
 }
 
 function verify_URL (url, index) {
-	request(url, function (err, res) {
-		if (res.statusCode === 200) {
-			console.log(url + ' works!');
-			verifiedUrls.push(url);
+  request(url, function (err, res) {
+    if (res.statusCode === 200) {
+      console.log(url + ' works!');
+      verifiedUrls.push(url);
             if (index == band_urls.length - 1) {
                 result();
             }
-		} else {
-			console.log(url + ' does not exist.');
+    } else {
+      console.log(url + ' does not exist.');
             if (index == band_urls.length - 1) {
                 result();
             }
-		}
-	});
+    }
+  });
 }
 
 function normalizer (string) {
-	var s = string.toLowerCase();
-	var normalizedString = '';
-	for (var i = 0; i < s.length; i++) {
-		if (normalizationMap[s[i]] !== undefined) {
-			normalizedString += normalizationMap[s[i]];
-		} else {
-			normalizedString += s[i];
-		}
-	}
-	return normalizedString;
+  var s = string.toLowerCase();
+  var normalizedString = '';
+  for (var i = 0; i < s.length; i++) {
+    if (normalizationMap[s[i]] !== undefined) {
+      normalizedString += normalizationMap[s[i]];
+    } else {
+      normalizedString += s[i];
+    }
+  }
+  return normalizedString;
 }
 
 bandlist(parseUrls);
