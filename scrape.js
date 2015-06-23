15b5ec9e66a4f3ba8ecafd9932dd70e59233b2a2
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-06-23 14:37:08 +0300

    Fix

--- scrape.js
+++ scrape.js
@@ -6,12 +6,11 @@ var parseURLs = require('./lib/parseURLs');
 var getLyrics = require('./lib/getLyrics');
 var getLyricURLs = require('./lib/getLyricURLs');
 var fs = require('fs-extra');
-var outputFileForAlbumURLs = './data/List_of_verified_album_URLs.txt';
-var urls = ['http://en.wikipedia.org/wiki/List_of_black_metal_bands,_0%E2%80%93K', 'http://en.wikipedia.org/wiki/List_of_black_metal_bands,_L%E2%80%93Z'];
+
 var dataDir = './data/';
 var lyricDir = './lyrics/';
-
-var bandNames = [];
+var outputFileForAlbumURLs = './data/List_of_verified_album_URLs.txt';
+var urls = ['http://en.wikipedia.org/wiki/List_of_black_metal_bands,_0%E2%80%93K', 'http://en.wikipedia.org/wiki/List_of_black_metal_bands,_L%E2%80%93Z'];
 
 // Scrape the list of BM bands from Wikipedia.
 scrapeBands(urls, function (data) {
@@ -26,7 +25,7 @@ scrapeBands(urls, function (data) {
       console.log('Directory ' + lyricDir + ' exists.');
     }
   });
-  bandNames = data;
+  var bandNames = data;
   // Parse band names into an array of URLs pointing to band pages in Darklyrics.com
   parseURLs(bandNames, function (result) {
     var unverifiedBandURLs = result.sort().reverse();
@@ -39,11 +38,7 @@ scrapeBands(urls, function (data) {
       });
       // Scrape the lyrics for each album and save them to file.
       var estimatedScrapeTime = (lyricURLs.length * 10 / 60 / 60).toFixed(1);
-      console.log('=========================================');
-      console.log('Scraping lyrics into files. Each album is retrieved at an interval of 10 seconds. This will take approximately ' + estimatedScrapeTime + ' hours.');
-      console.log('=========================================');
-      console.log('Created lyric files:');
-      
+      console.log('\nScraping lyrics into files. Each album is retrieved at an interval of 10 seconds. This will take approximately ' + estimatedScrapeTime + ' hours.\n');
       getLyrics(lyricURLs.reverse());
     });
   });
